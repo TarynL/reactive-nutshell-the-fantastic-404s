087cd0ccf8af560d4c88c08af6d7545e4053b740
Dakota Upchurch <cheyenneupchurch@hotmail.com>
2021-04-13 15:54:46 -0500

    save point

--- src/components/friends/AddFriendForm.js
+++ src/components/friends/AddFriendForm.js
@@ -2,9 +2,15 @@ import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { addFriend } from '../../modules/FriendManager';
 
+const currentUserId = sessionStorage.getItem("nutshell_user")
+
 export const AddFriendForm = () => {
     const [friends, setFriends] = useState({
         userId: 0,
-        currentUserId: 
+        currentUserId: currentUserId
     })
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    const history = useHistory();
 }
