a1686d7e04856efd0411a73aa8bfba56087e06a0
Laurel Morrison <lbentleym1992@gmail.com>
2021-04-14 14:11:30 -0400

    fixing timestamp

--- src/components/tasks/taskList.js
+++ src/components/tasks/taskList.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-//import the components we will need
 import { TaskCard } from './taskCard';
 import { getTaskByUserId, deleteTask, completeTask } from '../../modules/TaskManager';
 import { useHistory } from "react-router-dom"
