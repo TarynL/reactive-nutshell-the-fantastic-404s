e49f15784d075e3fb9b162d1b097212182d2ea00
Jeremiah Schugt <jeremiah.r.schugt@gmail.com>
2021-04-14 13:06:03 -0500

    Fix event card, and spelling

--- src/Modules/EventManager.js
+++ src/Modules/EventManager.js
@@ -3,3 +3,13 @@
 export const getEventsForUser = (userId) => {
     return fetch(`http://localhost:8088/events?userId=${userId}`).then(res => res.json())
 }
+
+export const deleteEventById = (id) =>{
+    return fetch(`http://localhost:8088/events?id=${id}`,{
+        method:"DELETE",
+        headers: {
+            "Conent-Type":"application/json"
+        }
+
+    }).then(res => res.json())
+}
--- src/components/events/eventCard.js
+++ src/components/events/eventCard.js
@@ -9,10 +9,18 @@ export const EventCard = ({ event}) => {
       <div className="card-content">
         <h3>Name: <span className="card-eventName">{event.eventName}</span></h3>
         <p>Date: {event.date}</p>
-        <h4>Locaiton: {event.location}</h4>
+        <h4>Location: {event.location}</h4>
+        <section>
         <button>
           Show Weather
         </button>
+        <button>
+          Edit
+        </button>
+        <button>
+          Delete
+        </button>
+        </section>
       </div>
     </div>
   );
