a561ec6338338419859960ccc601ef234005e19a
Taryn <Taryn@Flyings-MacBook-Pro.local>
2021-04-15 14:52:44 -0500

    messages view sees sent and received-fixed articleList

--- src/components/article/ArticleCard.js
+++ src/components/article/ArticleCard.js
@@ -2,27 +2,46 @@ import React from 'react';
 import "./ArticleCard.css"
 import { useHistory } from "react-router-dom";
 
-export const ArticleCard = ({ article, handleDeleteArticle }) => {
+export const ArticleCard = ({ article, handleDeleteArticle, loggedInUser }) => {
     const history = useHistory();
 
     return (
-        <div className="card">
-            <div className="card-content">
+        <>
+            {loggedInUser === article.userId ?
 
-                <h3>Article Title: <span className="card-title">
-                    {article.title}
-                </span></h3>
-                <p>Date: {article.date}</p>
-                <p>Synopsis: {article.synopsis}</p>
-                <a href = {article.url}>URL: {article.url}</a>
+                <div className="card">
+                    <div className="card-content">
 
-                <button type="button"
-                    onClick={() => history.push(`/articles/${article.id}/edit`)}>
-                    Edit
+                        <h3>Article Title: <span className="card-title">
+                            {article.title}
+                        </span></h3>
+                        <p>Date: {article.date}</p>
+                        <p>Synopsis: {article.synopsis}</p>
+                        <a href={article.url}>URL: {article.url}</a>
+
+                        <button type="button"
+                            onClick={() => history.push(`/articles/${article.id}/edit`)}>
+                            Edit
                 </button>
-                <button type="button" onClick={() => handleDeleteArticle(article.id)}>Delete</button>
+                        <button type="button" onClick={() => handleDeleteArticle(article.id)}>Delete</button>
 
-            </div>
-        </div>
-    )
+                    </div>
+                </div> :
+
+                <div className="card">
+                    <div className="card-content">
+
+                        <h3>Article Title: <span className="card-title">
+                            {article.title}
+                        </span></h3>
+                        <p>Date: {article.date}</p>
+                        <p>Synopsis: {article.synopsis}</p>
+                        <a href={article.url}>URL: {article.url}</a>
+
+                    </div>
+                </div>
+}
+</>   
+)
+    
 }
