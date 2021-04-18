54d18d76e6830ce4155d14c317d1d8c8b09b0752
Jeremiah Schugt <jeremiah.r.schugt@gmail.com>
2021-04-17 19:17:36 -0500

    Dates are sorted by most recent with effects

--- src/components/events/EventList.js
+++ src/components/events/EventList.js
@@ -7,8 +7,17 @@ import "./event.css"
 export const EventList = () => {
     const [events, setEvent] = useState([]);
     const history = useHistory();
+    // Sort array by date. 
+    const getSoonestEvent = (sortEvents) => {
+        let sortedByDate = sortEvents.sort(
+            (currentEntry, nextEntry) =>
+                Date.parse(currentEntry.eventDate) - Date.parse(nextEntry.eventDate)
+        )
+        return sortedByDate
+    }
+    // Get events from the Api then sort them by date and store them in state
     const getEvents = () => {
-        return getEventsForUser(sessionStorage.getItem("nutshell_user")).then(eventsFromApi => { setEvent(eventsFromApi) })
+        return getEventsForUser(sessionStorage.getItem("nutshell_user")).then(eventsFromApi => { setEvent(getSoonestEvent(eventsFromApi)) })
     }
     //Delete event by event id, then get all events and render
     const handleDeleteEvent = (id) => {
@@ -22,17 +31,25 @@ export const EventList = () => {
 
     // Check if any events exist for the user, if none then return nothing
     return (<>
+    <div>
         <Link to="/events/create"><button>Create Event</button></Link>
         <div className="eventList">
             {events.length > 0 ?
-                events.map(event => {
-                    return <EventCard
+                events.map((event, index) => {
+                    // Events are sorted by date, if it is the first element in the array then
+                    // set it's class name to first event
+                    // else later events
+                    let divClass = (index  === 0 )?"firstEvent":"laterEvents"
+                    return <div className={divClass}>
+                        <EventCard
                         key={event.id}
                         event={event}
                         handleDeleteEvent={handleDeleteEvent} />
+                        </div>
                 })
                 : ""}
         </div>
+    </div>
     </>)
 
 }
